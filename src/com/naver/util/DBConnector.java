bb9ec7f5291ea3423be3e8a5b872e1b2b39a4c95
youth2ee <sprirealee@gmail.com>
2019-11-04 15:55:17 +0900

    Update DBConnector.java

--- src/com/naver/util/DBConnector.java
+++ src/com/naver/util/DBConnector.java
@@ -10,8 +10,7 @@ public class DBConnector {
 		
 	 	String user = "hollys";
 	 	String password = "hollys";
-		/* String url = "jdbc:oracle:thin:@211.238.142.32:1521:xe"; */
-	 	String url = "jdbc:oracle:thin:@10.2.229.26:1521:xe";
+		String url = "jdbc:oracle:thin:@211.238.142.32:1521:xe";
 	 	String driver = "oracle.jdbc.driver.OracleDriver";
 	 	
 		Class.forName(driver);
